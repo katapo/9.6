553078bb556f6d658450691fb06c7de0ffd4e2c6
katapo <kata.pochec@gmail.com>
2017-11-14 12:04:25 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function(){
+	var table = document.getElementsByTagName('li');
+	var item = 'item ' + table.length;
+	list.innerHTML += '<li>' + item + '</li>';
+})
